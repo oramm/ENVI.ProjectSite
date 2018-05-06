b4dc541962ae46a5a36ef02030701c481b984cc8
oramm <marek@envi.com.pl>
2018-05-06 18:30:19 +0200

    Dodano przycisk edycji danych projektu.

--- Projects/ProjectDetailsView.js
+++ Projects/ProjectDetailsView.js
@@ -5,6 +5,11 @@ class ProjectDetailsView extends Popup {
     initialise(){
         this.setTittle("Informacje o projekcie");
         $('#actionsMenu').after(new ProjectDetailsCollection('projectDetailsCollection').$dom);
+        this.actionsMenuInitialise();
         this.dataLoaded(true);
+    }
+    actionsMenuInitialise(){
+        var newProjectButton = FormTools.createFlatButton('Edytuj dane ', ()=> window.open(projectsRepository.selectedItem.editUrl,'_blank'));
+        $('#actionsMenu').append(newProjectButton);
     }
 }
