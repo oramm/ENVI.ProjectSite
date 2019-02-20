0e3ae3d846640ddd96cd04c482d1e8cd32ce6479
oramm <marek@envi.com.pl>
2019-02-20 21:46:16 +0100

    zmiana limitu znaków Issues

--- Contracts/ContractDashboard/Issues/IssueModalEngineer.js
+++ Contracts/ContractDashboard/Issues/IssueModalEngineer.js
@@ -2,8 +2,8 @@ class IssueModalEngineer extends Modal {
     constructor(id, tittle, connectedResultsetComponent, mode){
         super(id, tittle, connectedResultsetComponent,mode);
         this.datePicker = new DatePicker(this.id + 'datePickerField','Data zgłoszenia', true);
-        this.descriptionReachTextArea = new ReachTextArea (this.id + '_descriptionReachTextArea','Opis', false, 500);
-        this.contractorsDescriptionReachTextArea = new ReachTextArea (this.id + '_contractorsdescriptionReachTextArea','Opis', false, 500);
+        this.descriptionReachTextArea = new ReachTextArea (this.id + '_descriptionReachTextArea','Opis', false, 1000);
+        this.contractorsDescriptionReachTextArea = new ReachTextArea (this.id + '_contractorsdescriptionReachTextArea','Opis', false, 1000);
         this.deadLinePicker = new DatePicker(this.id + 'deadLinePickerField','Termin wykonania', true);
         this.statusSelectField = new SelectField(this.id + 'statusSelectField', 'Status', true);
         this.statusSelectField.initialise(IssuesSetup.statusNames);
