353f57ca8f9f3711e768043f889b2def72a86171
Marek Gazda <33371875+oramm@users.noreply.github.com>
2024-02-17 16:57:23 +0100

    entities poprawki

--- docs/Entities/Modals/EntityValidationSchema.js
+++ docs/Entities/Modals/EntityValidationSchema.js
@@ -27,29 +27,26 @@ exports.makeEntityValidationSchema = void 0;
 const Yup = __importStar(require("yup"));
 const commonFields = {
     name: Yup.string()
-        .required('Podaj nazwę')
-        .min(3, 'Nazwa musi mieć co najmniej 3 znaki')
-        .max(150, 'Nazwa może mieć maksymalnie 150 znaków'),
-    address: Yup.string()
-        .default('')
-        .max(250, 'Adres może mieć maksymalnie 250 znaków'),
+        .required("Podaj nazwę")
+        .min(3, "Nazwa musi mieć co najmniej 3 znaki")
+        .max(150, "Nazwa może mieć maksymalnie 150 znaków"),
+    address: Yup.string().max(250, "Adres może mieć maksymalnie 250 znaków"),
     taxNumber: Yup.string()
-        .default('')
-        .test('len', 'Numer podatkowy musi mieć dokładnie 10 lub 13 znaków', val => val.length === 10 || val.length === 13),
-    www: Yup.string()
-        .default('')
-        .max(150, 'WWW może mieć maksymalnie 150 znaków'),
+        .nullable()
+        .test("len", "Numer podatkowy musi mieć dokładnie 10 lub 13 znaków", (val) => val ? val.length === 10 || val.length === 13 : true),
+    www: Yup.string().max(150, "WWW może mieć maksymalnie 150 znaków"),
     email: Yup.string()
-        .default('')
-        .matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, 'Nieprawidłowy format email')
-        .max(80, 'Email może mieć maksymalnie 80 znaków'),
-    phone: Yup.string()
-        .default('')
-        .max(25, 'Telefon może mieć maksymalnie 25 znaków'),
+        .nullable()
+        .matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, {
+        message: "Nieprawidłowy format email",
+        excludeEmptyString: true,
+    })
+        .max(80, "Email może mieć maksymalnie 80 znaków"),
+    phone: Yup.string().max(25, "Telefon może mieć maksymalnie 25 znaków"),
 };
 function makeEntityValidationSchema(isEditing) {
-    return (Yup.object().shape({
+    return Yup.object().shape({
         ...commonFields,
-    }));
+    });
 }
 exports.makeEntityValidationSchema = makeEntityValidationSchema;
