53c16c029ebeb1dbf25a8c21a1d5a2a48692d171
Marek Gazda <33371875+oramm@users.noreply.github.com>
2026-02-17 09:53:18 +0100

    Update kill-webpack.ps1

--- scripts/kill-webpack.ps1
+++ scripts/kill-webpack.ps1
@@ -4,7 +4,8 @@ if ($connections) {
     foreach ($conn in $connections) {
         try {
             Stop-Process -Id $conn.OwningProcess -Force -ErrorAction Stop
-        } catch {
+        }
+        catch {
             Write-Host "Unable to stop PID $($conn.OwningProcess): $_" -ForegroundColor Yellow
         }
     }
